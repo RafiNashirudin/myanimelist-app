0f062938a8f9c83421033849abcdf33f7cf107f3
BelahanDeveloper <belahandev@gmail.com>
2023-11-20 19:24:59 +0700

    Trick Nested Data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,22 @@
 import AnimeList from '@/components/AnimeList';
 import Header from '@/components/AnimeList/Header';
-import { getAnimeResponse } from './libs/api-libs';
+import { getAnimeResponse, getNestedAnimeResponse } from '@/libs/api-libs';
+import { data } from 'autoprefixer';
 
 const Page = async () => {
   const topAnime = await getAnimeResponse('top/anime', 'limit=8');
+  let recommendedAnime = await getNestedAnimeResponse('recommendations/anime', 'entry');
+  recommendedAnime = { data: recommendedAnime.slice(0, 8) };
 
   return (
     <>
       <section>
         <Header title="Paling Populer" linkTitle="Lihat Semua" linkHref="/populer" />
         <AnimeList api={topAnime} />
+      </section>
+      <section>
+        <Header title="Rekomendasi" />
+        <AnimeList api={recommendedAnime} />
       </section>
     </>
   );
